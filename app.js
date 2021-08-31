fee238fe8dc87d284b61a68332cddb9c07c8ed86
Igor G <darkzodiac55@gmail.com>
2021-08-31 14:14:13 +0200

    more functionality added

--- app.js
+++ app.js
@@ -6,28 +6,43 @@ class trackerDial  {
         this.price = 40000
     }
 
-    upPrice (delay) {
+    /* upPrice (delay) {
         setInterval(()=>{
             this.price += 5
             this.id.innerText = this.price
         },delay)
-    }
+    } */
 }
 
 class mainDial extends trackerDial{
     constructor() {
         super()
         this.getPrice()
-        this.upPrice(2000)
+        /* this.upPrice(2000) */
     }
     id = document.querySelector(".contMain h1")
     delay = 2000
 }
 
+//////////////////currency selectable
+
 class init {
     static initDials() {
     this.rMainDial = new mainDial()
     }
+
+    static async getAPrice() {
+        try {
+            let response = await axios.get('https://api.cryptonator.com/api/ticker/btc-eur')
+            let price = response.data.ticker.price
+            let EUprice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(price);
+            console.log(typeof EUprice);
+            this.rMainDial.id.innerText = EUprice
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
 init.initDials()
+init.getAPrice()
