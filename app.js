e9175d28efeec26d4c213f79584d82221d6ba72c
Igor G <darkzodiac55@gmail.com>
2021-09-04 10:41:06 +0200

    basic currency change fn

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ class trackerDial {
 
     upPrice(delay) {
         setInterval(() => {
-            this.getAPrice()
+            this.getAPrice(this.cur)
         }, delay)
     }
 
-    async getAPrice() {
+    async getAPrice(cur) {
         try {
-            let response = await axios.get('https://api.cryptonator.com/api/ticker/btc-eur')
+            let response = await axios.get(`https://api.cryptonator.com/api/ticker/btc-${cur}`)
             this.price = response.data.ticker.price
-            let EUprice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(this.price);
+            let EUprice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: `${cur}` }).format(this.price);
             this.addLis(EUprice)
 
         } catch (error) {
@@ -35,13 +35,26 @@ class trackerDial {
 class mainDial extends trackerDial {        //////can potentially add individual "Dials" for each currency/per various days
     constructor() {
         super()
-        this.getAPrice()
+        this.getAPrice('eur')
         this.upPrice(this.delay)
         
     }
     id = document.querySelector(".contMain h1")
-    delay = 20000
+    delay = 2000000
+    cur = 'eur'
 
+}
+
+class Header {
+    constructor() {
+        this.addLisCur()
+    }
+    btnCur = document.querySelector(".currency")
+    addLisCur() {
+        this.btnCur.addEventListener('click', ()=>{
+            init.currencySetter('usd')
+        })
+    }
 
 }
 
@@ -58,16 +71,27 @@ class mainDial extends trackerDial {        //////can potentially add individual
 
 //////////////////currency selectable
 
+class DOMhelper {
+    
+}
 
 class init {
     static initDials() {
         this.rMainDial = new mainDial()
     }
+    static initHeader() {
+        this.header = new Header()
+    }
     static priceSetter(price) {
         this.rMainDial.id.innerText = price
+    }
+    static currencySetter(cur) {
+        this.rMainDial.cur = cur
+        this.rMainDial.getAPrice(cur)
     }
 
 }
 
 init.initDials()
+init.initHeader()
 
